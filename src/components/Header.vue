7db8b59f48f4b953a6bca27b7c3aeec4a91ba1ff
onii <fabianmann9@gmail.com>
2023-01-26 13:23:31 +0545

    flex wrap

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex justify-around space-x-44 text-base bg-purple-800 text-white">
+    <div class="flex flex-wrap justify-around space-x-44 text-base bg-purple-800 text-white">
         <router-link class="p-3 hover:text-purple-900 hover:bg-white cursor-pointer" to="/">Home</router-link>
         <router-link class="p-3 hover:text-purple-900 hover:bg-white cursor-pointer" to="/add">Add Book</router-link>
         <a @click="logout" class="p-3 mr-2 hover:text-purple-900 hover:bg-white cursor-pointer">Log out</a>
